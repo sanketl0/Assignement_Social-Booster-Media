ff1024a6609005aa4ba07fafb4dec40029111bd3
sanketl0 <sanketlodhe666@gmail.com>
2025-12-10 17:17:36 +0530

    created change

--- backend/TaskManagement/settings.py
+++ backend/TaskManagement/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
